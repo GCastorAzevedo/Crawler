8fa7dd9933c2d2612930042995bd9abef0b0dff2
Gabriel Castor <gabrielcastor1991@gmail.com>
2018-07-23 01:18:00 -0300

    Update agents.crawl.js

--- agents.crawl.js
+++ agents.crawl.js
@@ -1,9 +1,12 @@
 const CDPParams = require('./packages/CarbonDisclosureProject/agent.crawler');
 const crawler = require('./packages/CarbonDisclosureProject/CarbonDisclosureProject');
 
-module.exports = {
+// agents contains all crawler subscriptions. Each agent has a crawler function together with its parameters
+const agents = {
     "CarbonDisclosureProject": {
         params: CDPParams,
         crawler: crawler,
     }
 }
+
+module.exports = agents;
